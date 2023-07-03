5945a5a47d5356b80d4ce3468eaaf100b6f3911f
Ronan <gronansilva@gmail.com>
2023-07-03 11:04:29 -0300

    Função de login

---
+++ CRUD/functions.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+
+class Funcs:
+    def Login(self, user, pasw):
+        try:
+            self.db_con = mysql.connector.connect(host='localhost', user=user, password=pasw, database='basevendas')
+            print("Conectado!")
+            self.cursor = self.db_con.cursor()
+        except mysql.connector.Error as error:
+            if error.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Servidor indisponível!")
+            if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Usuário ou senha incorreto!")
+        else:
+            print("Concluído!")
+
+user = input("Digite seu usuário:")
+pasw = input("Digite sua senha:")
+Funcs.Login(Funcs, user, pasw)
